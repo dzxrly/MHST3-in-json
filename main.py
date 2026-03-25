958e94a3975811817d1b967836a269f095430571
dzxrly2 <dzxrly2@gmail.com>
2026-03-25 18:40:42 +0800

    add Enums_Internal.json

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ from __future__ import annotations
 
 import argparse
 import json
+import os
 
 from user3_exporter import User3Exporter
+
+ENUM_UNUSED_KEY = "value__"
+
+
+def export_enums_internal(dump_json: dict) -> dict:
+    enums_internal = {}
+    for key, value in dump_json.items():
+        if isinstance(value, dict):
+            # check if is "parent"
+            obj = dump_json[key]
+            if "parent" in obj and obj["parent"] == "System.Enum":
+                val = {}
+                for _k, _v in obj["fields"].items():
+                    if _k != ENUM_UNUSED_KEY:
+                        val[_k] = _v["default"]
+                enums_internal[key] = val
+    return enums_internal
 
 
 def main() -> None:
@@ -40,6 +58,18 @@ def main() -> None:
     else:
         tree_depth = int(args.tree_depth)
 
+    # Export Enums_Internal.json from input_dir/il2cpp_dump.json
+    print("Exporting Enums_Internal.json from il2cpp_dump.json...")
+    assert os.path.exists(os.path.join(args.input_dir, "il2cpp_dump.json")), "il2cpp_dump.json not found"
+    with open(os.path.join(args.input_dir, "il2cpp_dump.json"), "r", encoding="utf-8") as f:
+        il2cpp_dump = json.load(f)
+    enums_internal = export_enums_internal(il2cpp_dump)
+    with open(os.path.join(args.output_dir, "Enums_Internal.json"), "w", encoding="utf-8") as f:
+        json.dump(enums_internal, f, ensure_ascii=False, indent=2)
+    print("Exported Enums_Internal.json from il2cpp_dump.json. Done.")
+
+    # Export .user.3 files to JSON
+    print("Exporting .user.3 files to JSON...")
     exporter = User3Exporter(
         user3_root=args.input_dir,
         schema_dir=args.schema_dir,
@@ -47,7 +77,7 @@ def main() -> None:
         tree_depth=tree_depth,
     )
     result = exporter.run()
-    print("Done:", json.dumps(result, ensure_ascii=False))
+    print("Exported .user.3 files to JSON. Done:", json.dumps(result, ensure_ascii=False))
 
 
 if __name__ == "__main__":
